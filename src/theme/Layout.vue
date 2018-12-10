2fb04702a95367f7774fcd43c622850047b35435
Jessica Hathaway <jhathaway.5sun@gmail.com>
2018-12-10 11:54:32 -0800

    fix version mismatch, move template into Vue component

---
+++ src/theme/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="nav has-shadow">
+      <div class="container"></div>
+      <a href="/">
+        <img src="http://bit.ly/vue-img"
+             alt="Vue SPA">
+      </a>
+    </nav>
+    <section class="main-section section"></section>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Follow us on
+          <a href="https://twitter.com/hathawayj3ss"
+             target="_blank">Twitter</a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
