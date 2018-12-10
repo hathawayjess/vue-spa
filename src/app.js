8eb20a23e07632e64a1c8a7011a168426aff3c33
Jessica Hathaway <jhathaway.5sun@gmail.com>
2018-12-10 11:39:53 -0800

    add eslint, create Vue component for app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
-import Vue from "vue";
+import Vue from 'vue'
+
+Vue.component('app', {
+  template: `
+      <div id="app">
+        <nav class="nav has-shadow">
+          <div class="container"></div>
+            <a href="/">
+            <img src="http://bit.ly/vue-img"
+                  alt="Vue SPA">
+            </a>
+        </nav>
+        <section class="main-section section"></section>
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              Follow us on
+              <a href="https://twitter.com/hathawayj3ss"
+              target="_blank">Twitter</a>
+            </div>
+          </div>
+        </footer>
+      </div>
+`
+})
 
 const app = new Vue({
-  data: {
-    hello: "Hallo"
-  },
-  template: '<div id="app"> {{ hello }} </div>div>'
-});
+  render: h => h('app')
+})
 
-export { app };
+export { app }
